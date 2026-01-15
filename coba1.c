1131395ed6f9cda0b805432f54bfedf5034f7a0f
ryanpurnomo83 <ryanpurnomo83@gmail.com>
2026-01-15 19:29:52 +0700

    second commit

--- coba1.c
+++ coba1.c
@@ -1,2 +1,22 @@
 #include <stdio.h>
 
+int recursive(int a, int b);
+
+int main() {
+    int x = 10;
+    int y = 5;
+
+    recursive(x, y);
+
+    return 0;
+}
+
+int recursive(int a, int b) {
+    if (a == 0) {
+        return b; // base case
+    }
+
+    printf("a = %d, b = %d\n", a, b);
+
+    return recursive(a - 1, b);
+}
